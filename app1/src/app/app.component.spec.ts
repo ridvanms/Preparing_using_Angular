1c7f0d75a214c15984bc4b7fc29877375483749d
Ridvan <ridvans@abv.bg>
2023-05-30 15:51:25 +0300

    learning separatedly

--- app1/src/app/app.component.spec.ts
+++ app1/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'RecipeBook'`, () => {
+  it(`should have as title 'app1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('RecipeBook');
+    expect(app.title).toEqual('app1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('RecipeBook app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('app1 app is running!');
   });
 });
