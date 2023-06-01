81ea5c0dcb7f148e6be3db4ff3249142a751982f
Ridvan <ridvans@abv.bg>
2023-06-01 08:49:49 +0300

    adding app4

--- app4/src/app/app.component.spec.ts
+++ app4/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'app3'`, () => {
+  it(`should have as title 'app4'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('app3');
+    expect(app.title).toEqual('app4');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('app3 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('app4 app is running!');
   });
 });
